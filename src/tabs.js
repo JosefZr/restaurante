dec71e7da38ec3f0b46cf5ff35758fb833511b8d
zeraibiyoucef <zeraibiyoucef1@gmail.com>
2023-08-29 17:00:14 +0100

    finching the resposiveness of the tabs and menu

--- src/tabs.js
+++ src/tabs.js
@@ -11,6 +11,7 @@ const creatTab = ()=>{
     const div2 = document.createElement('div');
     const div3 = document.createElement('div');
     const divCart = document.createElement('div');
+    const burger = document.createElement('div');
     const divLogo = document.createElement('div');
 
     div1.setAttribute("id",'home-btn')
@@ -24,6 +25,7 @@ const creatTab = ()=>{
     div2.classList.add('tab');
     div3.classList.add('tab');
     divCart.classList.add('cart');
+    burger.classList.add('burger');
     divLogo.classList.add("logo");
 
     //set text content for the tabs
@@ -31,7 +33,8 @@ const creatTab = ()=>{
     div2.textContent='Menu';
     div3.textContent='Contact';
     divCart.innerHTML='<i class="fa-regular fa-cart-plus"></i>';
-    divLogo.innerHTML='<img src="images/pizza-box.png">'
+    burger.innerHTML=`<i class="fa-solid fa-burger" ></i>`;
+    divLogo.innerHTML='<img src="images/pizza-box.png">';
 
     //append the divs to the content div
     links.appendChild(div1)
@@ -39,6 +42,7 @@ const creatTab = ()=>{
     links.appendChild(div3)
     header.appendChild(links)
     header.appendChild(divCart)
+    header.appendChild(burger)
     header.prepend(divLogo)
     content.appendChild(header);
 
@@ -75,10 +79,38 @@ const creatTab = ()=>{
         clearBack();
         removeAfter(); // Remove the ::after pseudo-element
     });
-    
-    // ...
-    
 
+    burger.addEventListener("click", ()=>{
+        if(links.classList.contains("active"))
+            links.classList.remove("active");
+        else    
+            links.classList.add("active"); // Toggle the 'active' class on 'Links'
+        
+    });
+    document.addEventListener("click", (event) => {
+        const isBurgerClicked = burger.contains(event.target); // Check if burger was clicked
+        const isActive = links.classList.contains("active"); // Check if Links has 'active' class
+    
+        // If burger wasn't clicked and Links has 'active' class, remove 'active' class
+        if (!isBurgerClicked && isActive && !event.target.classList.contains("active")) {
+            links.classList.remove("active");
+        }
+    });
+    function removeActiveClass() {
+        links.classList.remove("active");
+    }
+    const mediaQuery = window.matchMedia("(min-width: 480px)"); // Change the breakpoint as needed
+
+    mediaQuery.addEventListener("change", (event) => {
+        if (event.matches) {
+            // Screen size is now greater than or equal to the breakpoint
+            removeActiveClass();
+        }
+    });
+    // Initial check to see if the screen size is already larger than the breakpoint
+    if (mediaQuery.matches) {
+        removeActiveClass();
+    }
       // Clear the background color when leaving the "Menu" section
     div1.addEventListener('click', clearBack);
     div3.addEventListener('click', clearBack);
@@ -87,7 +119,6 @@ function addAfter() {
     const pageContent = document.querySelector('.page-content');
     pageContent.classList.add('with-after');
 }
-
 function removeAfter() {
     const pageContent = document.querySelector('.page-content');
     pageContent.classList.remove('with-after');
@@ -99,5 +130,4 @@ function clearContent(){
         content.removeChild(pageContent);
     }
 }
-
 export default creatTab;
